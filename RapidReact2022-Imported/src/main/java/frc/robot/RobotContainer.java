aef2961a7b30462cff5f20733176d771e6e1f67e
John Lee <2003jjl@gmail.com>
2022-01-16 16:44:29 -0500

    Updated Gradle version and imported project to 2022 firmware

--- RapidReact2022-Imported/src/main/java/frc/robot/RobotContainer.java
+++ RapidReact2022-Imported/src/main/java/frc/robot/RobotContainer.java
@@ -5,7 +5,7 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.GenericHID;
-import edu.wpi.first.wpilibj.SlewRateLimiter;
+import edu.wpi.first.math.filter.SlewRateLimiter;
 import edu.wpi.first.wpilibj.XboxController;
 import frc.robot.commands.ExampleCommand;
 import frc.robot.commands.TankDrive;
@@ -13,7 +13,7 @@ import frc.robot.subsystems.DriveTrain;
 import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 
-import edu.wpi.first.wpilibj.SlewRateLimiter;
+import edu.wpi.first.math.filter.SlewRateLimiter;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
