0bef81b9f05e8c8047caa5d46f6b78f86905ae79
John Lee <2003jjl@gmail.com>
2022-02-19 11:03:59 -0500

    completed the intake motors and pistons

--- RapidReact2022-Imported-Imported/src/main/java/frc/robot/RobotContainer.java
+++ RapidReact2022-Imported-Imported/src/main/java/frc/robot/RobotContainer.java
@@ -10,11 +10,14 @@ import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.math.filter.SlewRateLimiter;
 import edu.wpi.first.wpilibj.XboxController;
 import frc.robot.commands.ArcadeDrive;
+import frc.robot.commands.IntakeMotor;
+import frc.robot.commands.IntakePiston;
 import frc.robot.commands.Shooter;
 import frc.robot.commands.SlowMode;
 import frc.robot.commands.TankDrive;
 import frc.robot.commands.Autonomous.AutonomousMode;
 import frc.robot.subsystems.DriveTrain;
+import frc.robot.subsystems.IntakeSubsystem;
 import frc.robot.subsystems.ShooterSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 
@@ -37,6 +40,7 @@ public class RobotContainer {
   private final ShooterSubsystem m_shooterSubsystem = new ShooterSubsystem();
   // private final Shooter m_shooter = new Shooter();
   private final AutonomousMode m_autonomousMode = new AutonomousMode(m_drivetrain);
+  private final IntakeSubsystem m_intakeSubsystem = new IntakeSubsystem();
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
@@ -75,6 +79,11 @@ public class RobotContainer {
       new Shooter(
         () -> m_controller1.getRightTrigger(), m_shooterSubsystem)
     );
+
+    m_intakeSubsystem.setDefaultCommand(
+      new IntakeMotor(
+        () -> m_controller1.getRightStickY(), m_intakeSubsystem)
+    );
   }
 
   /**
@@ -87,6 +96,7 @@ public class RobotContainer {
     // l_controller0.l_button3.toggleWhenPressed(new SlowMode());
     // l_controller0.l_trigger1.toggleWhenPressed(new Shooter(-0.6, m_shooterSubsystem)); //0.759
     m_controller0.xButton.toggleWhenPressed(new SlowMode());
+    m_controller1.yButton.whenHeld(new IntakePiston(m_intakeSubsystem));
   }
 
   /**
---
+++ RapidReact2022-Imported-Imported/src/main/java/frc/robot/commands/IntakeMotor.java
@@ -0,0 +1,45 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import java.util.function.DoubleSupplier;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.IntakeSubsystem;
+
+public class IntakeMotor extends CommandBase {
+  private final IntakeSubsystem m_intakeSubsystem;
+  private final DoubleSupplier m_input;
+
+  /** Creates a new IntakeMotor. */
+  public IntakeMotor(DoubleSupplier input, IntakeSubsystem intakeSubsystem) {
+    m_intakeSubsystem = intakeSubsystem;
+    m_input = input;
+    
+    addRequirements(m_intakeSubsystem);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    m_intakeSubsystem.suck(m_input.getAsDouble());
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    m_intakeSubsystem.stopIntake();
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
